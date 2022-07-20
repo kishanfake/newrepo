95ac388c2afd98a81e19e51d1d51826f78f69820
kishanfake <kishan@whiteorangesoftware.com>
2022-07-20 11:02:18 +0530

    library add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import CButton from './components/CButton/cButton';
-import CInput from './components/CInput/CInput';
-import Form from './pages/form';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
-
-
-
   return (
-    <div style={{ maxWidth: 500, justifyContent: "center", margin: 'auto',marginTop:100 }} className="App">
-      <Form />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
